fa0ec63533fdf0903b7c3a38134dcbdddd1f24f8
Samuele Francesconi <sam@MacBookPro.homenet.telecomitalia.it>
2025-02-19 18:28:52 +0100

    daisy

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 const Calendar = require('./services/Calendar.js');
-
 const calendar = new Calendar('./service-account.json');
 
-calendar.getEvents().then((events) => {
-    console.log(events);
-}).catch((error) => {
-    console.error('Error fetching events:', error);
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
+client.login(process.env.DISCORD_TOKEN);
+
+
+client.on("messageCreate", async (message) => {
+    if(message.content == '!daisy') {
+        calendar.getEvents()
+            .then(events => {
+                events.map(event => message.author.send('Alle ore ' + event.start.dateTime + ' dobbiamo provvedere per ' + event.summary));
+            })
+    }
+       
+})

b0debc94f806b2844f974b7b84c5387dace0882f
Samuele Francesconi <sam@MacBookPro.homenet.telecomitalia.it>
2025-02-20 22:45:50 +0100

    daisy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Calendar = require('./services/Calendar.js');
 const DiscordBot = require('./services/DiscordBot.js');
+const dayjs = require('dayjs');
+
 
 const calendar = new Calendar('./service-account.json');
 const bot = new DiscordBot();
@@ -11,7 +13,8 @@ bot.client.on("messageCreate", async (message) => {
         const events = await calendar.getEvents();
        
         if(events) {
-            return events.map(event => message.author.send('Alle ore ' + event.start.dateTime + ' dobbiamo provvedere per ' + event.summary));
+            //using this for my dog's appointments
+            return events.map(event => message.author.send(dayjs(event.start.dateTime).format('DD-MM-YYYY HH:mm:ss') + ' abbiamo appuntamento per ' + event.summary));
         };
 
         return message.author.send('Ops.... qualcosa è andato storto'); 
